9e6134a593a445bcf6444dc5e8be7161b6933a09
Zurplupz <jlda_5991@tutanota.com>
2021-04-10 23:22:10 -0500

    Made most gallery properties guarded

--- app/gallery.php
+++ app/gallery.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class gallery extends Model
 {
+	protected $fillable = ['title','favorited','rating'];
+
+	function __construct()
+	{
+		parent::__construct();
+
+		$table = \Schema::getColumnListing('galleries');
+
+		$this->guarded = array_filter($table, function ($v) {
+			return !in_array($v, $this->fillable);
+		});
+	}
+
 	protected $hidden = ['gallery_group','gallery_tagging'];
 
 	public function gallery_group()
