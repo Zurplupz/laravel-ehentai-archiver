25073838769f544c71a4d09f25d46f0d407ed02e
Zurplupz <jlda_5991@tutanota.com>
2021-04-25 00:20:37 -0500

    Access gallery download form and GP - Credit exchange page

--- app/Http/ApiClients/ExhentaiClient.php
+++ app/Http/ApiClients/ExhentaiClient.php
@@ -12,6 +12,7 @@ class ExhentaiClient extends Client
 {
 	protected $api_url = 'https://exhentai.org/api.php';
 	protected $archives_url = 'https://exhentai.org/archiver.php';
+	protected $exchange_url = 'https://e-hentai.org/exchange.php?t=gp';
 	
 	function __construct()
 	{
@@ -36,6 +37,7 @@ class ExhentaiClient extends Client
 		return $this->request($this->api_url, compact('json'), 'POST');
 	}
 
+	/* direct access to download link */
 	public function requestArchive(array $query, string $resolution='resampled')
 	{
 		$this->result_type = 'text';
@@ -61,10 +63,34 @@ class ExhentaiClient extends Client
 		return $response;
 	}
 
+	public function requestArchiveForm(array $query)
+	{
+		$this->result_type = 'text';
+
+		$response = $this->request($this->archives_url, compact('query'), 'POST');
+		
+		$this->result_type = 'json';
+
+		return $response;
+	}
+
+	/* deprecated */
 	public function downloadGallery(string $url, string $save_path)
 	{
 		$this->result_type = 'text';
 
-		return $response->request($url, ['sink' => $save_path]);
+		return $this->request($url, ['sink' => $save_path]);
+	}
+
+	public function exchangePage()
+	{
+		$this->result_type = 'text';		
+
+		$this->cookies = CookieJar::fromArray([
+		    'ipb_member_id' => env('EXHENTAI_MEMBER_ID'),
+		    'ipb_pass_hash' => env('EXHENTAI_PASS_HASH'),
+		], '.e-hentai.org');
+
+		return $this->request($this->exchange_url);
 	}
 }
